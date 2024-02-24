1ae521fb7ead5cc8af7110e9afb254c683b9575e
Habibullo <Habibulloit3gmail.com>
2024-02-24 20:06:36 +0500

    react-test

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from 'react'
+
+function App() {
+  const [name, setName] = useState(`John`)
+
+  const [events, setEvents] = useState([
+    {title: "Hello my name is John", id: 1},
+    {title: "John birthday 2000", id: 2},
+    {title: "John last name is Doe", id: 3},
+  ])
+
+  const handleCLick = () => {
+       setName('Doe')
+  }
+
+  return (
+    <div className="App">
+      <h1>My name is {name}</h1>
+      <button onClick={handleCLick}>Change name</button>
+      {events.map((event) => {
+        return (
+          <div key={event.id}>
+            <h2>{event.title}</h2>
+          </div>
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
